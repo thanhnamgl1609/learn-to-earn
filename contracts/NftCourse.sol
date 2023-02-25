4222a144fcb10a892f56d5e8a7a83a7a8a41f876
thanhnamgl1609 <thanhnamgl1609@gmail.com>
2023-02-25 19:36:39 +0700

    refactor code

--- contracts/NftCourse.sol
+++ contracts/NftCourse.sol
@@ -3,209 +3,77 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./NftGraduation.sol";
+import "./School.sol";
 
 contract NftCourse is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
-	enum CourseType {
-		General,
-		Foundation,
-		Specialized,
-		SpecializedElective,
-		Elective
-	}
+    struct NftItem {
+        uint256 tokenId;
+        string studentId;
+        uint256 courseId;
+    }
 
-	enum CourseStatus {
-		InProgress,
-		Complete
-	}
-	private const REGISTERED_TEACHER_LIMIT = 100;
+	School private immutable _schoolContract;
 
-	struct Course {
-		uint256 id;
-		address teacherAddress;
-		uint256 credits;
-		uint256 remainNftCounts;
-    	uint256 maxNftCounts; // to limit the nft course minted
-		string courseGroup; // course group id, course name: uri
-		CourseType type;
-		CourseStatus status;
-	}
+    Counters.Counter private _tokenIds;
+    NftItem[] private _allNftItems;
+    mapping(uint256 => uint256) private _idToNftIndex; // tokenId =>NftItem 
+    // mapping(string => mapping(CourseType => uint256[])) private _completedCourse;
+    mapping(string => bool) private _usedTokenURIs;
+    
+    constructor(School schoolContract) ERC721("CourseNFT", "CNFT") {
+        _schoolContract = schoolContract;
+    }
 
-	struct Teacher {
-		uint256 registeredDate;
-		address registeredAddress;
-		string metadata;
-	}
+    function getNftCourse(uint256 tokenId) public view returns (NftItem memory) {
+        uint256 tokenIndex = _idToNftIndex[tokenId];
 
-	struct NftItem {
-		uint256 tokenId;
-		string studentId;
-		uint256 courseId;
-	}
+        return _allNftItems[tokenIndex];
+    }
 
-	NftGraduation private immutable _graduationContract;
- 
-	// Course
-	Counters.Counter private _courseCount;
-	Counters.Counter private _courseIds;
-	Course[] private _allCourses;
-	mapping(uint256 => uint256) private _idToCourseIndex;
+    // function mintToken(
+    //     string memory tokenURI,
+    //     string memory studentId,
+    //     uint256 courseId
+    // ) public onlyTeacher(courseId) returns (uint256)
+    // {
+        // require(!_usedTokenURIs[tokenURI], "URI has already existed");
+        // _tokenIds.increment();
 
-	// Metadata = Course group
-	string[] private _courseGroups;
-	mapping(string => bool) private _registeredCourseGroups;
+        // uint256 newTokenId = _tokenIds.current();
+        // _safeMint(msg.sender, newTokenId);
+        // _setTokenURI(newTokenId, tokenURI);
+        // _createNftItem(newTokenId, studentId, courseId);
+        // _usedTokenURIs[tokenURI] = true;
 
-	// Nft Course
-	// studentId => type => NftCourse?
-	NftItem[] private _allNftItems;
-	mapping(string => uint256) private _idToNftIndex; // tokenId =>NftItem 
-	mapping(string => mapping(type => uint256[])) private _completedCourse;
+        // return newTokenId;
+    // }
 
-	// Registered teacher
-	Teacher[] _allRegisteredTeachers;
-	Teacher[] _allApprovedTeachers;
-	mapping(address => uint256) private _addressToRegisteredTeacherIndex;
-	mapping(address => uint256) private _addressToApprovedTeacherIndex;
-	// mapping(address => Teacher) private _rejectedTeacher;
- 
-	modifier onlyTeacher(uint256 courseId) {
-		require(teacherAddressOf(courseId).registeredAddress, 'access denied');
-	}
- 
-	event TeacherRegistered(string metadata);
- 
-	constructor() ER721("CourseNFT", "CNFT") {
-		_graduationContract = new NftGraduation();
-	}
-  
-	/* Teacher section: start
-	Can create smart contract inherit ChainLink to manage teacher
-	*/
-	function teacherAddressOf(uint256 courseId) {
-		Course memory course = _idToCourse[courseId].teacherId;
+    function _createNftItem(
+        uint256 tokenId,
+        string memory studentId,
+        uint256 courseId
+    ) private {
+        // NftItem memory nftItem = NftItem(tokenId, studentId, courseId);
+        // _allNftItems.push(nftItem);
+        // _idToNftIndex[tokenId] = _allNftItems.length;
+        // studentId => 
+    }
 
-		return _idToTeacherAddress[teacherId];
-	}
+    // function exchangeNftGradution(
+    //     string memory studentId,
+    //     uint256[] memory tokenIds
+    // ) public allowGetNftGraduation {
+    //     bool isAllBelongsToStudent = true;
+    //     uint256 tokenIdLength = tokenIds.length;
+    //     // mapping(uint256 => uint256) tokenIds;
 
-	function registerTeacher(string metadata) public { // Use payable? (? Lost)
-		require(_addressToRegisteredTeacherIndex[msg.sender] == 0, 'You has registered');
-		require(_addressToApprovedTeacher[teacherAddress] == 0, 'You has been approved');
-		require(_allRegisteredTeachers.length < REGISTERED_TEACHER_LIMIT, 'Too many waiting request. Please try again later.');
-		// Idea:  chainlink to call recheckRegisteredTeacher();
-
-		uint256 currentIndex = _allRegisteredTeachers.length;
-		_allRegisteredTeachers[currentIndex] = new Teacher(now(), msg.sender, metadata);
-		_addressToApprovedTeacherIndex[msg.sender] = currentIndex;
-
-		// EVENT
-	}
-
-	function approveTeacher(address teacherAddress) public onlyOwner { // Send back (? Lost)
-		require(_addressToApprovedTeacherIndex[teacherAddress] == 0, 'Teacher has been registered');
-		require(_addressToRegisteredTeacherIndex[teacherAddress] > 0, 'Teacher has not been registered');
-
-		uint256 teacherIndex = _addressToRegisteredTeacherIndex[teacherAddress];
-		uint256 lastTeacherIndex = _allRegisteredTeachers.length;
-		Teacher memory teacher = _allRegisteredTeachers[teacherIndex];
-
-		// remove teacher from register list
-		delete _addressToRegisteredTeacherIndex[teacherAddress];
-		if (teacherIndex != lastTeacherIndex) {
-			Teacher memory lastTeacher = _allRegisteredTeachers[lastTeacherIndex];
-			_allRegisteredTeachers[teacherIndex] = lastTeacher;
-			_addressToRegisteredTeacherIndex[lastTeacher.registeredAddress] = teacherIndex;
-		}
-		delete _allRegisteredTeachers[lastTeacherIndex];
-
-		// add teacher to approve list
-		uint256 currentIndex = _allApprovedTeachers.length();
-		_addressToApprovedTeacherIndex[teacherAddress] = currentIndex;
-		_allApprovedTeachers.push(teacher);
-
-		// EVENT
-	}
-
-	function removeTeacher(address teacherAddress) public onlyOwner {
-		// Check if teacher currently has class?
-		uint256 teacherIndex = _addressToApprovedTeacherIndex[teacherAddress];
-		uint256 lastTeacherIndex = _allApprovedTeachers.length;
-		delete _addressToApprovedTeacherIndex[teacherAddress];
-		if (teacherIndex != lastTeacherIndex) {
-			Teacher memory lastTeacher = _allApprovedTeachers[lastTeacherIndex];
-			_allApprovedTeachers[teacherIndex] = lastTeacher;
-			_addressToApprovedTeacherIndex[lastTeacher.registeredAddress] = teacherIndex;
-		}
-		delete _addressToApprovedTeacherIndex[lastTeacherIndex];
-
-		// EVENT
-	}
-
-	// Idea:  Use chainlink to remove registered teacher after 30 days if he/she is not registered
-	// function recheckRegisteredTeacher() public onlyOwner {
-		// require(_approvedTeacher[msg.sender].registeredDate == 0)
-	// }
-
-	function rejectRegisteredTeacher() public onlyOwner {
-		// Idea: Add address to blacklist in a particular time => prevent address from registering in a while?
-		uint256 teacherIndex = _addressToRegisteredTeacherIndex[teacherAddress];
-		uint256 lastTeacherIndex = _allRegisteredTeachers.length;
-		delete _addressToRegisteredTeacherIndex[teacherAddress];
-		if (teacherIndex != lastTeacherIndex) {
-			Teacher memory lastTeacher = _registeredTeacherCount[lastTeacherIndex];
-			_allRegisteredTeachers[teacherIndex] = lastTeacher;
-			_addressToRegisteredTeacherIndex[lastTeacher.registeredAddress] = teacherIndex;
-		}
-		delete _addressToRegisteredTeacherIndex[lastTeacherIndex];
-
-		// EVENT
-	}
-	// Teacher section: end
-	
-
-	function createCourse(
-		address teacherAddress,
-		uint256 credits,
-    	uint256 remainNftCounts,
-		uint256 maxNftCounts,
-		string courseGroup, // -> course group?
-		CourseType type
-	) public onlyOwner returns (Course) {
-		require(_approvedTeacher[teacherAddress] > 0, 'Teacher must register before assignment');
-
-		_courseCount.increment();
-		_courseIds.increment();
-		uint256 currentId = _courseIds.current();
-		uint256 currentIndex = _allCourses.length;
-		_idToCourseIndex[currentId] = currentIndex;
-		_allCourses.push(
-			new Course(
-				currentId,
-				teacherAddress,
-				credits,
-				remainNftCounts,
-				maxNftCounts,
-				courseGroup,
-				type,
-				CourseStatus.InProgress
-			)
-		);
-
-		// EVENT
-	}
-
-	function completeCourse(
-		uint256 courseId,
-	) public onlyTeacher(courseId) {
-		uint256 couresIndex = _idToCourseIndex[courseId];
-		_allCourses[courseId] = CourseStatus.Complete;
-	}
-
-	/*
-		?? should check an appropriate time?
-	*/
-	function mintToken() public onlyTeacher(courseId) {
-
-	}
+    //     for (uint256 index; index < tokenIdLength; ++index) {
+            
+    //         isAllBelongsToStudent = false;
+    //     }
+        
+    //     // _graduationContract.mintToken();
+    // }
 }
--- contracts/NftGraduation.sol
+++ contracts/NftGraduation.sol
@@ -5,29 +5,30 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./School.sol";
 
 contract NftGraduation is ERC721URIStorage, Ownable {
-	struct NftItem {
-		uint256 studentId;
-		uint256 tokenId;
-	}
- 
-	address private immutable _courseContract;
- 
-	// Nft Graduation
-	NftItem[] private _allNftItem;
-	mapping(string => NftItem) private _idToNftItem;
+    struct NftItem {
+    	uint256 studentId;
+    	uint256 tokenId;
+    }
 
+	School private immutable _schoolContract;
 
-	modifier onlyCourseContract {
-		require(msg.sender === _courseContract, 'access denied');
-	}
+    // // Nft Graduation
+    NftItem[] private _allNftItem;
+    mapping(string => NftItem) private _idToNftItem;
 
-	constructor() ERC721("GraduationNft", "GNFT") {
-		_courseContract = msg.sender;
-	}
+    // modifier onlyCourseContract {
+    // 	require(msg.sender == _courseContract, 'access denied');
+    // 	_;
+    // }
 
-	function mintToken() public onlyCourseContract {
+    constructor(School schoolContract) ERC721("GraduationNft", "GNFT") {
+        _schoolContract = schoolContract;
+    }
 
-	}	
+    // function mintToken() public onlyCourseContract {
+
+    // }
 }
